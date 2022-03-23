e79a94a9044801187a2fb13e3d290bf0c6fa2ba1
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-03-23 15:59:16 +0200

    homeWork3 start

---
+++ homeWork3/index.js
@@ -0,0 +1,11 @@
+// let name = prompt("Ваше имя?"),
+// age = prompt('Ваш возраст?');
+ 
+// alert(`Приветствую, ${name}, Ваш возраст в годах: ${age}`);
+
+
+const name = prompt("Имя"),
+age = prompt("Возраст");
+
+console.log(`привет ${name} тебе ${age}`);
+alert(`привет ${name} tebe ${age}`);
