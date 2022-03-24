5aa6d65010696a7d95e6a1459f487ec049138247
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-03-24 15:04:25 +0200

    update js

--- homeWork3/index.js
+++ homeWork3/index.js
@@ -1,8 +1,29 @@
+let firstValue = parseInt(prompt("Значение 1")),
+secondValue = parseInt(prompt("Значение 2")),
+operator = prompt("Оператор"),
+result = parseInt();
+switch (operator) {
+    case "+": {
+        result = firstValue + secondValue;
+        break;
+    }
+    case "-": {
+        result = firstValue - secondValue;
+        break
+    }
+    case "*": {
+        result = firstValue * secondValue;
+        break
+    }
+    case "/": {
+        result = firstValue / secondValue;
+    }
+        
+    default: {
+        alert(`Не верно указан оператор`);
+        break;
+    }
+}
 
-const firstValue = +prompt("Первое число"),
-secondValue = +prompt("Второе число"),
-operator = prompt("Оператор");
-
-let calcResult = Number()
-
-alert(`${firstValue} ${operator} ${secondValue}`);
+console.log(`${firstValue} ${operator} ${secondValue} = ${result} `);
+  
