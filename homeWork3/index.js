fa2a5d5538200c2db3a516b2e1a3d70e5ae4588c
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-03-24 16:31:02 +0200

    fix js file

--- homeWork3/index.js
+++ homeWork3/index.js
@@ -1,7 +1,6 @@
 let firstValue = parseInt(prompt("Значение 1")),
 secondValue = parseInt(prompt("Значение 2")),
-operator = prompt("Оператор"),
-result = parseInt();
+operator = prompt("Оператор");
 switch (operator) {
     case "+": {
         result = firstValue + secondValue;
@@ -24,6 +23,14 @@ switch (operator) {
         break;
     }
 }
+if (Boolean(firstValue) === false) {
+    alert('empty first value field');
+}
+if (Boolean(secondValue) === false) {
+    alert('empty second value field');
+}
+
+
 
 console.log(`Результат: ${firstValue} ${operator} ${secondValue} = ${result} `);
   
