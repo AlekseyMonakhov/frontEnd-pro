767fb6b34cc3dd0483b7608aac62367b1723326f
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-03-27 23:08:30 +0300

    change hm4

--- homeWork3/index.js
+++ homeWork3/index.js
@@ -1,42 +1,49 @@
-let firstValue = parseInt(prompt("Значение 1")),
-secondValue = parseInt(prompt("Значение 2")),
-operator = prompt("Оператор");
-
-if (!firstValue) {
-    alert('empty first value field');
-    firstValue = null;
-}
-if (!secondValue) {
-    alert('empty second value field');
-    secondValue = null;
-}
-
-switch (operator) {
-    case "+": {
-        result = firstValue + secondValue;
-        break;
-    }
-    case "-": {
-        result = firstValue - secondValue;
-        break
-    }
-    case "*": {
-        result = firstValue * secondValue;
-        break
-    }
-    case "/": {
-        result = firstValue / secondValue;
-        break
-    }
-        
-    default: {
-        alert(`Мы не можем выполнить действие с оператором ${operator}`);
-        break;
-    }
-}
 
 
 
 
-console.log(`Результат: ${firstValue} ${operator} ${secondValue} = ${result} `);
+
+
   
+let cyclesSum = parseInt(prompt('введите количество вычислений которое Вы хотите сделать'));
+while (cyclesSum) {
+    
+    let firstValue = parseInt(prompt("Значение 1")),
+    secondValue = parseInt(prompt("Значение 2")),
+    operator = prompt("Оператор");
+
+    if (!firstValue) {
+        alert('empty first value field');
+        firstValue = null;
+    }
+    if (!secondValue) {
+        alert('empty second value field');
+        secondValue = null;
+    }
+
+    switch (operator) {
+        case "+": {
+            result = firstValue + secondValue;
+            break;
+        }
+        case "-": {
+            result = firstValue - secondValue;
+            break
+        }
+        case "*": {
+            result = firstValue * secondValue;
+            break
+        }
+        case "/": {
+            result = firstValue / secondValue;
+            break
+        }
+            
+        default: {
+            alert(`Мы не можем выполнить действие с оператором ${operator}`);
+            break;
+        }
+    }
+        console.log(`Результат: ${firstValue} ${operator} ${secondValue} = ${result} `);
+        cyclesSum--;
+}
