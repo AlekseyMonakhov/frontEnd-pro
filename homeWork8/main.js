945c00b10ac82922708c9ede01e6624fb35b10cd
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-04-15 00:40:23 +0300

    fix

--- homeWork8/main.js
+++ homeWork8/main.js
@@ -2,30 +2,43 @@
 cars = [];
 
 function carCreate (num) {
-  
-  let myCars = { 
-    mark: ["BWM", "Toyota", "MAZDA"],
-    model: ["X5", "X6", "X7"],
-    year: ["2020", "2015", "2010"],
-    price: ["High", "Low", "Medium"],
-    color: ["black", "white", "pink"],
+  availabelCars = {
+    model: ["X1", "X2", "X3"],
+    brand: ["Tesla", "BMW", "Ford"],
+    price: ["Large", "Medium", "Small"],
+    color: ["red", "blue", "green"],
+    age: ["1year", "3years", "5years"],
   };
-  let myCar = {};
   do {
-    for (key in myCars) {
-      do {
-        myCar[key] = prompt(`Chose from available options what ${key}: ${myCars[key]}`);
-        if(myCars[key].includes(myCar[key])) {
-          alert(`${key} aded`);
-        }else {
-          alert(`invalid ${key}: availabel are next - ${myCars[key]}`);
-        }
+    myCar ={};
+    for (let key in availabelCars) {
+      let value = prompt(`enter ${key} value`, `${availabelCars[key]}`);
+      if (availabelCars[key].includes(value)) {
+        myCar[key] = value;
+      } else {
+        alert(`Wrong value: ${key}`)
       }
-      while(!myCars[key].includes(myCar[key]));
     }
-    cars.push(myCar);
-  }while (--num);
+    cars.push(myCar);    
+
+  } while (--num); 
 };
   
 carCreate(2);
 console.log(cars);
+//   do {
+//     let myCar = {};
+//     for (key in myCars) {
+//       do {
+//         myCar[key] = prompt(`Chose from available options what ${key}: ${myCars[key]}`);
+//         if(myCars[key].includes(myCar[key])) {
+//           alert(`${key} aded`);
+//         }else {
+//           alert(`invalid ${key}: availabel are next - ${myCars[key]}`);
+//         }
+//       }
+//       while(!myCars[key].includes(myCar[key]));
+//     }
+//     cars.push(myCar);
+//   }while (--num);
+// }
