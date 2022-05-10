ee23c463a34be6033c40406ca44ae6c0f0b5ec1b
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-05-10 12:07:08 +0300

    hw 17

--- homeWork17/main.js
+++ homeWork17/main.js
@@ -1,24 +1,45 @@
-let arr = [4,1,4,4,12,23,43,24,1,34,123,4,123,4,1234,];
-
-// function arrFilt(arr) {
-//   let filt =[];
-//   arr.forEach(value => {
-//     if(!filt.includes(value)){
-//       filt.push(value);
-//     }
-//   })
-//   return filt;
-// }
-
+// first tast
+let arr = [1,2,14,14,15,133,2,8,12,19,8,13,123,1,2,3,4,5,6,7,7,8,9];
 function arrFilt(arr) {
-  filt =[];
-  arr.filter((el, index, arr) => {
-    if (!filt.includes(el)) {
-      filt.push(el);
-    }
-  })
-  return filt;
+  return arr
+  .filter((el, index, arr) => arr.indexOf(el) === index)
+  .sort((a,b) => a - b);
 }
 
 
 console.log(arrFilt(arr));
+// second tast
+
+Number.prototype.plus = function (value) {
+	return this + value;
+}
+
+Number.prototype.minus = function (value) {
+	return this - value;
+}
+console.log((2).plus(3).minus(1));
+
+// ----------------------- anagram
+let firstWord = `Ромашка, нитка, сон`;
+let secondWord = 'Мошкара, нос, ткани';
+
+function anagram(firstWord, secondWord) {
+  firstWord = firstWord.toLowerCase().split('').sort().join(``);
+  secondWord = secondWord.toLowerCase().split('').sort().join(``);
+  return firstWord === secondWord ? `Anagram`: `not Anagram`;
+}
+console.log(anagram(firstWord, secondWord));
+
+// --------------words
+const words = ['orange', 'orange', 'orange', 'banana', 'banana', 'grapefruit', 'banana', 'grapefruit', 'banana'];
+
+function solution(words) {
+
+    let valuesMap = new Map();
+
+    words.forEach((elem) => valuesMap.set(elem, valuesMap.has(elem) ? valuesMap.get(elem) + 1 :  1));
+    let arr =  [...valuesMap.entries()].sort((a, b) => b[1] - a[1]);
+    return arr.map(value => value[0]);
+}
+
+console.log(solution(words));
