1f9cfebeb7d15f8c358f4f3c9a368fd2c71574db
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-05-21 00:00:12 +0300

    calc

--- homeWork14/main.js
+++ homeWork14/main.js
@@ -5,19 +5,18 @@ let number = ``;
 let result = 0;
 let operator = [`/`, `*`, `+`, `-`,`=`];
 
-// формула не должна начинатся или заканчиватся оператором 
-// не должно быть два оператора подряд или (оператор не может быть отдельным елементом массива)
-// number может заканчиватся оператором , в намбер может быть только одна точка
 function clearAll(event) {
     if(event.target.classList.contains(`ac`)) {
         math.length = 0;
         result = 0;
         number = ``;
         console.log(math, number);
+        output.textContent = math.join(``);
     }
 }
 function calc(event) {
     let inputValue = event.target.textContent;
+    
     if(inputValue.search(/[^0-9*/+-.=]/mi)!= -1) {
         return;
     }
@@ -27,6 +26,7 @@ function calc(event) {
     };
     
     number += inputValue;
+    output.textContent = number;
     if(!result) {
         if(operator.includes(number)) {
         number = ``;
@@ -64,8 +64,10 @@ function calc(event) {
         math[0] = result;
         console.log(result);
         console.log(math);
+        output.textContent = math.join(``);
     }
 };
+
 
 
 btnContainer.addEventListener(`click`, (event) => {
