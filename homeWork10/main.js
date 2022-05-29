64bc6a498130c73830aad961bde04f7d48f81093
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-05-29 22:32:05 +0300

    hw 9 practies

--- homeWork10/main.js
+++ homeWork10/main.js
@@ -1,27 +1,72 @@
 const arr = [1,2,3,4,5,6,7,8,9,10];
 
-function chunk(arr, chunkSize) {
-  newArr = [];
-  if (!chunkSize) {
-    alert(`Invalid , value cannot = ${chunkSize}`);
-  } else if (chunkSize === 1) {
-    return arr;
+
+function chunk(list = [], chunkSize = list.length) {
+  if (chunkSize) {
+    return list.reduce((res, curr, index)=>{
+      if(index % chunkSize === 0) {
+        res.push([]);
+      }
+      res[res.length-1].push(curr); 
+      return res;
+    },[]);
+  } else {
+    return 'Пользователь, ты не прав!';
   }
-  else {
-    while (arr.length > 0) {
-      newArr.push(arr.splice(0, chunkSize))
-    }
-    return newArr;
-  }
+}
+
+
+// function chunk(list = [], chunkSize = list.length) {
+//   if (chunkSize) {
+//     return list.reduce((result, currentNum, index) => {
+//       if (index % chunkSize === 0) {
+//         result.push([]);
+//       }
+//       result[result.length-1].push(currentNum);
+//       return result;
+//     }, []);
+//   } else {
+//     return 'Пользователь, ты не прав!';
+//   }
+// }
+
+
+
+// function chunk(arr, chunk) {
+//   newArr = [];
+//   if(chunk > arr.length) {
+//     return `noup`;
+//   }
+//   if(chunk === 1) {
+//     return arr;
+//   }
+//   for (let index = 0; index < arr.length; index += chunk) {
+//     newArr.push(arr.slice(index, index+chunk));
+//   }
+//   return newArr;
+// }
+
+// function chunk(arr, chunkSize) {
+//   newArr = [];
+//   if (!chunkSize) {
+//     alert(`Invalid , value cannot = ${chunkSize}`);
+//   } else if (chunkSize === 1) {
+//     return arr;
+//   }
+//   else {
+//     while (arr.length > 0) {
+//       newArr.push(arr.splice(0, chunkSize))
+//     }
+//     return newArr;
+//   }
 
   // for (let i = 0; i < arr.length; i += chunkSize) {
   //   newArr.push(arr.slice(i, i+chunkSize));
   // }
   // return newArr;
-}
+// }
 
 
-console.log(chunk(arr, 3));
-
+console.log(chunk(arr,7));
 
 // Первый метод не меняет исходный массив , второй меняет 
