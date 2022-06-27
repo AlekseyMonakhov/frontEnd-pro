75e29aa9c899adc7d7c44492fa79061a2c55220b
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-06-27 14:13:06 +0300

    delte try

--- homeWork19/main.js
+++ homeWork19/main.js
@@ -55,12 +55,12 @@ class ListItem {
 		this.maxCount = maxCount;
 	}
 	addItem(itemName, itemCount, itemUnit) {
-		try {
 			if (Object.values({ itemName, itemCount, itemUnit }).includes(``) || Object.values({ itemName, itemCount, itemUnit }).includes(undefined)) {
 				let x = Object.entries({ itemName, itemCount, itemUnit }).filter(el => el.includes(``) || el.includes(undefined));
 				let emptyFields = x.flat().filter(el => el !== undefined);
+				let inputedValue = Object.values({ itemName, itemCount, itemUnit }).filter(el => el !== undefined && el !== ``);
 				if (emptyFields.length) {
-					throw new EmptyFields(`${emptyFields.join(`, `)}  ${emptyFields.length > 1 ? `fields are` : `field is`}  empty`);
+					throw new EmptyFields(`${inputedValue} not aded: ${emptyFields.join(`, `)}  ${emptyFields.length > 1 ? `fields are` : `field is`}  empty`);
 				}
 			};
 			if (this.items.length >= this.maxCount) {
@@ -72,15 +72,6 @@ class ListItem {
 			let newItem = new Item(itemName, itemCount, itemUnit, this.id);
 			this.items.push(newItem);
 			this.id++;
-		} catch (err) {
-			if (err instanceof EmptyFields) {
-				let inputedValue = Object.values({ itemName, itemCount, itemUnit }).filter(el => el !== undefined && el !== ``);
-				err.message = `${inputedValue} not added ${err.message}`;
-				throw err;
-			}else{
-				throw err;
-			};
-		}
 	};
 	removeItem(id) {
 			let removeItem = this.items.find(el => el.itemId === id);
